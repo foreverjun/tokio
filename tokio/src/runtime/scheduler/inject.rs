85a5b975f4225b1253c42365149d2927ed547d03
matvey <kalashnikov.matvey.m@gmail.com>
2025-04-16 23:40:16 +0300

    fix fmt

--- tokio/src/runtime/scheduler/inject.rs
+++ tokio/src/runtime/scheduler/inject.rs
@@ -16,8 +16,8 @@ cfg_rt_multi_thread! {
     mod rt_multi_thread;
 }
 
-mod metrics;
 mod inject_overflow;
+mod metrics;
 
 /// Growable, MPMC queue used to inject new tasks into the scheduler and as an
 /// overflow queue when the local, fixed-size, array queue overflows.
